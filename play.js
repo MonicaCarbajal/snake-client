6825bcea01d19a420eeae5fae245f150e16c7e23
Monica Carbajal <monica.carbajal2@gmail.com>
2023-07-29 16:05:30 -0600

    adding more changes

--- play.js
+++ play.js
@@ -1,4 +1,26 @@
 const connect = require("./client");
 
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    // If the input is ctrl + c (SIGINT), terminate the game and exit
+    process.exit();
+  }
+};
+
+
 console.log("Connecting ...");
 connect();
+setupInput();
