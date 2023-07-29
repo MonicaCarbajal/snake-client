5dc231b0085d28a19e28dd845f44ea74bf946dc5
Monica Carbajal <monica.carbajal2@gmail.com>
2023-07-29 14:06:16 -0600

    adding client and play

---
+++ client.js
@@ -0,0 +1,28 @@
+// establishes a connection with the game server
+
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541,
+  
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log("Server says:", data);
+  });
+
+  conn.on("connect", () => {
+    conn.write("Successfully connected to game server");
+    conn.write("Name: MCU");
+  });
+
+  return conn;
+};
+
+
+module.exports = connect;
